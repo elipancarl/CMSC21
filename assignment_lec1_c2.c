09a8bd34fac503e5b5ea2b0e24b326addbd42738
elipancarl <100710785+elipancarl@users.noreply.github.com>
2022-03-04 01:01:16 +0800

    Add files via upload

---
+++ assignment_lec1_c2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    a = 87;
+    b = 15;
+    c = a - b;
+    printf("The difference between %d and %d is %d.\n", a, b, c);
+    return 0;
+}
